e6f1f72c95cfc0d563b8e81441d938ff5c361ef9
Bijusrt <54570266+Bijusrt@users.noreply.github.com>
2020-02-01 19:00:23 +0530

    Add files via upload

---
+++ encryption.py
@@ -0,0 +1,38 @@
+chars =         ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+shifted_chars = ['c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b']
+def encrypt_message(plain_msg):
+    encrypted_msg =''
+    for character in plain_msg:
+        if character in chars:
+            char_index = chars.index(character)
+            new_char = shifted_chars[char_index]
+            encrypted_msg = encrypted_msg + new_char
+        else:
+            encrypted_msg = encrypted_msg + character
+    print encrypted_msg
+def decrypt_message(encrypted_msg):
+    decrypted_msg = ""
+    for character in encrypted_msg:
+        if character in shifted_chars:
+            char_index = shifted_chars.index(character)
+            new_char = chars[char_index]
+            decrypted_msg = decrypted_msg + new_char
+        else:
+            decrypted_msg = decrypted_msg + character
+    print decrypted_msg
+flag = True
+whilesflag = True
+choice = raw_input("What do you want to do? 1. Encrypt a message 2. Decrypt a message  Enter `e` or `d` respectively!")
+if choice == 'e':
+    plain_message = raw_input("Enter message to encrypt??")
+    encrypt_message(plain_message)
+elif choice == 'd':
+    encrypted_msg = raw_input("Enter message to decrypt?")
+    decrypt_message(encrypted_msg)
+else:
+    play_again = raw_input("Do you want to try agian or Do you want to exit? (Y/N)")
+    while True:
+	    if play_again == 'Y':
+	        continue
+	    elif play_again == 'N':
+	        break
